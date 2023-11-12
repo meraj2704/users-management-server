7d95e0937f8657902b129a9b52a9d5ca0e9d6b82
merajHossain <mh1669101@gmail.com>
2023-11-12 20:32:19 +0600

    get data form client and send response to client

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json())
 
 const users = [
   {id:1, name:'Rahat', gmail:'rahat@gmail.com'},
@@ -23,6 +24,10 @@ app.get('/users', (req,res)=>{
 app.post('/users', (req,res) =>{
   console.log('Api is hitting');
   console.log(req.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser);
 })
 
 app.listen(port, ()=>{
