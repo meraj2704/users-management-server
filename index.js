1dc7183ff9d21a709f49f499d51e518565e811bd
merajHossain <mh1669101@gmail.com>
2023-11-12 19:44:43 +0600

    request for post data

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ app.get('/users', (req,res)=>{
   res.send(users);
 })
 
+app.post('/users', (req,res) =>{
+  console.log('Api is hitting');
+  console.log(req.body);
+})
+
 app.listen(port, ()=>{
   console.log(`Srver is running on Port : ${port}`);
 })
